b624938ebeed62efebd9e1335779679f92d47757
Shivom Bhargava <Shivom.Bhargava@gds.ey.com>
2024-07-19 15:17:43 +0530

    Version 1 of data and Code

---
+++ src/data_ingestion.py
@@ -0,0 +1,7 @@
+import os
+import pandas as pd
+
+df = pd.read_csv(r'C:\Users\XM891JZ\Downloads\Ecommerce Customers.csv')
+df = df[df['Length of Membership'] > 3]
+df = df.drop(columns = ['Email','Address','Avatar'])
+df.to_csv('./data/customers.csv')
